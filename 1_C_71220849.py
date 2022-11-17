c9d0695b87331db88976b7d5e3af2896acacb0e9
EzraNahumury <117893407+EzraNahumury@users.noreply.github.com>
2022-11-17 16:22:22 +0700

    Add files via upload

---
+++ 1_C_71220849.py
@@ -0,0 +1,21 @@
+namadepan = "Cassandra"
+namabelakang = "Charlotte"
+matakuliah = "logika matematika"
+grup = "a"
+input("Masukkan nama anda:Cassandra Charlotte")
+input("Masukkan mata kuliah:Logika matematika")
+input("Masukkan grup:A")
+print("hallo",namabelakang)
+print(namabelakang,"tergabung dalam kelas",matakuliah,"pada grup",grup)
+
+namadepan = "Alaska sebastian"
+namabelakang = "Dirgantara"
+matakuliah = "Teknologi Komputer"
+grup = "D"
+input("Masukkan nama anda:Alaska Sebastian Dirgantara")
+input("Masukkan mata kuliah:Teknologi Komputer")
+input("Masukkan grup:D")
+print("hallo",namabelakang)
+print(namabelakang,"tergabung dalam kelas",matakuliah,"pada grup",grup)
+
+
---
+++ 2_C_71220849.py
@@ -0,0 +1,16 @@
+kecepatan = int(input("Masukkan kecepatan tempuh"))
+waktu = int(input("Masukkan waktu yang diperlukan"))
+#rumus
+bensin = kecepatan*waktu//10
+biaya = bensin*15000
+print("teman anda mengisi bensin sebanyak",bensin,"liter")
+print("biaya yang dikeluarkan untuk mengisi bensin adalah",biaya)
+
+
+kecepatan = int(input("Masukkan kecepatan tempuh"))
+waktu = int(input("Masukkan waktu yang diperlukan"))
+#rumus
+bensin = kecepatan*waktu//10
+biaya = bensin*15000
+print("teman anda mengisi bensin sebanyak",bensin,"liter")
+print("biaya yang dikeluarkan untuk mengisi bensin adalah",biaya)
